3bf341019424de2a74855a5e82a1622b949869ec
alexamarki <amarkin2006@gmail.com>
2023-04-22 17:04:26 +0300

    added requirements, got rid of redundant imports

--- forms/advanced.py
+++ forms/advanced.py
@@ -1,6 +1,5 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, BooleanField, SelectField, SubmitField
-from wtforms.validators import DataRequired
 
 
 class AdvancedForm(FlaskForm):
